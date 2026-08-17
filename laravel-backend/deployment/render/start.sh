5128af4ea2ee1f35999b31c99f7b75e513ea31b5
Mohammad Sheikhmous <shekhmosjaguar979@gmail.com>
2026-08-17 13:49:21 +0300

    Run database seeder during Render deployment

--- laravel-backend/deployment/render/start.sh
+++ laravel-backend/deployment/render/start.sh
@@ -8,6 +8,7 @@ set -eu
 
 php artisan config:clear
 php artisan migrate --force
+php artisan db:seed --force
 php artisan config:cache
 
 exec php artisan serve --host=0.0.0.0 --port="${PORT:-10000}"
