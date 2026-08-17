a4168f4ed8763bb5ab5a570efd9d86e38b59bbb7
Mohammad Sheikhmous <shekhmosjaguar979@gmail.com>
2026-08-17 14:39:11 +0300

    Some Fixes and Seeder Files

--- laravel-backend/database/seeders/DoctorSeeder.php
+++ laravel-backend/database/seeders/DoctorSeeder.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DoctorSeeder extends Seeder
 {
